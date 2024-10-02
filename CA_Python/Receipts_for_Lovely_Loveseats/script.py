375d306be77b4046aa87c84e71914180fd211861
abbie_codes <126886615+a-wisbey@users.noreply.github.com>
2024-10-02 18:39:08 +0100

    Update script.py

---
+++ CA_Python/Receipts_for_Lovely_Loveseats/script.py
@@ -0,0 +1,18 @@
+lovley_loveseat_description = '''
+Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white.
+'''
+
+lovely_loveseat_price = 254.00
+
+stylish_settee_description = '''
+Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black.
+'''
+stylish_settee_price = 180.50
+
+luxurious_lamp_description = '''
+Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade.
+'''
+
+luxurious_lamp_price = 52.15
+
+sales_tax = .088
