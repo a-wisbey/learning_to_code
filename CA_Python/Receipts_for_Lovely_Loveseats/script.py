2b56eeb6e1b290a4e98bc2f79ddd7ff886e0b0ec
abbie_codes <126886615+a-wisbey@users.noreply.github.com>
2024-10-02 18:46:43 +0100

    Add the receipt text.py

--- CA_Python/Receipts_for_Lovely_Loveseats/script.py
+++ CA_Python/Receipts_for_Lovely_Loveseats/script.py
@@ -16,3 +16,39 @@ Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade.
 luxurious_lamp_price = 52.15
 
 sales_tax = .088
+
+customer_one_total = 0
+
+customer_one_itemization = ""
+
+#Our customer has decided they are going to purchase our Lovely Loveseat! Add the price to customer_one_total
+customer_one_total += lovely_loveseat_price
+
+#Let’s start keeping track of the items our customer purchased. Add the description of the Lovely Loveseat to customer_one_itemization
+customer_one_itemization += lovley_loveseat_description
+
+#Our customer has also decided to purchase the Luxurious Lamp! Let’s add the price to the customer’s total.
+customer_one_total += luxurious_lamp_price 
+
+#Let’s keep the itemization up-to-date and add the description of the Luxurious Lamp to our itemization
+customer_one_itemization += luxurious_lamp_description
+
+#They’re ready to check out! Let’s begin by calculating sales tax. Create a variable called customer_one_tax and set it equal to customer_one_total times sales_tax.
+customer_one_tax = (customer_one_total * sales_tax)
+
+#Add the sales tax to the customer’s total cost.
+customer_one_total += sales_tax
+
+#Let’s start printing up their receipt! Begin by printing out the heading for their itemization. Print the phrase "Customer One Items:".
+phrase1 = "Customer One Items: "
+print(phrase1)
+print(customer_one_itemization)
+
+phrase2 = "Customer One Total:"
+print(phrase2)
+print (customer_one_total)
+
+
+
+
+
